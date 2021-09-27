8621845567a52ddda1d67578ddb37fad9d1f3112
asraful196 <asraful.01110@gmail.com>
2021-09-28 02:06:30 +0600

    Milstone all project in one folder

--- simple-cart-react-conceptual-3/src/components/Cart/Cart.js
+++ simple-cart-react-conceptual-3/src/components/Cart/Cart.js
@@ -1,35 +1,24 @@
 import React from 'react';
 
 const Cart = (props) => {
-
-    const {cart}= props || {}
-
-
-    const totalReducer=(prevValue,currentValue)=>prevValue+currentValue.price
-
-   const total=cart.reduce(  totalReducer   ,  0).toFixed(2)
-
-   const tax=0.1*total;
-
-      const totalPrice=total+tax
-
-    
-
-
-    console.log(cart)
+    const { cart } = props || {};
+    const totalReducer = (prevValue, currentValue) => prevValue + currentValue.price;
+    const total = cart.reduce(totalReducer, 0)
+    const tax = total * 0.1;
+    const totalPrice = total + tax
     return (
         <div>
-              <h3><i class="fas fa-shopping-cart"></i>
-                 {cart.length}
-               </h3>
-                <h2>{total}</h2>
-                <h3> Tax: {tax}</h3>
-                <h1>total Price: {totalPrice}</h1>
-               <ul>
-                   {
-                       cart.map( product=> <li>{product.name}</li>)
-                   }
-               </ul>
+            <h3><i className="fas fa-shopping-cart "></i>
+                {cart.length}
+            </h3>
+            <h4>Product price:  {total.toFixed(2)}</h4>
+            <h3> Tax:  {tax.toFixed(2)}</h3>
+            <h4>GrandTotal Price:  {totalPrice.toFixed(2)}</h4>
+            <ul>
+                {
+                    cart.map(product => <li>{product.name}</li>)
+                }
+            </ul>
         </div>
     );
 };
--- simple-cart-react-conceptual-3/src/components/Product/Product.js
+++ simple-cart-react-conceptual-3/src/components/Product/Product.js
@@ -6,20 +6,21 @@ const Product = (props) => {
 
     return (
      <div className="col-md-6">
-            <div class="card mb-3" style={ {"max-width": "540px"}}>
-        <div class="row g-0">
-          <div class="col-md-5">
-            <img src={img} class="img-fluid rounded-start" alt="..."/>
+            <div className="card mb-3" style={ {"max-width": "540px"}}>
+        <div className="row g-0">
+          <div className="col-md-5">
+            <img src={img} className="img-fluid rounded-start" alt="..."/>
           </div>
-          <div class="col-md-7">
-            <div class="card-body">
-              <h5 class="card-title"><small>{name}</small></h5>
-              <p class="card-text">
+          <div className="col-md-7">
+            <div className="card-body">
+              <h5 className="card-title"><small>{name}</small></h5>
+              <p className="card-text">
               <small>$   {price}</small>
               </p>
-              <p class="card-text">
+              <p className="card-text">
               <small> {stock}  left</small>
               </p>
+              <p><small>catagory: {category}</small></p>
               <button 
               onClick={ ()=> props.handleAddProduct(props.product)}
               className="btn btn-primary"> 
